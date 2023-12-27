d80d04a46a002029f2a6bc317a573a50b36bf7cc
Adam Quinton <8365885+zvakanaka@users.noreply.github.com>
2023-12-26 18:34:52 -0800

    clean up

--- src/vindriktning-oled-no-wifi.cpp
+++ src/vindriktning-oled-no-wifi.cpp
@@ -18,11 +18,11 @@ uint32_t statusCheckPreviousMillis = millis();
 
 CircularBuffer<char,126> circBuf; // uses 538 bytes
 
-#define DHTPIN 14     // Digital pin connected to the DHT sensor
+#define DHTPIN 14     // digital pin connected to the DHT sensor
 #define DHTTYPE DHT22 // DHT 22 (AM2302)
 DHT dht(DHTPIN, DHTTYPE);
 
-// Yes, this is hacky but it is working in time to make it a Christmas gift
+// Yes, this is hacky but it is working in time to make it a Christmas gift!
 char identifier[24];
 char avgPM25[24];
 char fahrenheit[24];
@@ -46,10 +46,7 @@ void setup() {
   u8g2.setContrast(0);
   u8g2.setFont(u8g2_font_unifont_t_symbols);
   u8g2.drawGlyph((128 - 12) / 2, 40, 0x23f3);
-
   u8g2.setFont(u8g2_font_simple1_te);
-  // u8g2.clearBuffer();
-  // u8g2.drawStr(0,20,"Hello from esp8266-vindriktning-particle-sensor");
   u8g2.sendBuffer();
 
   delay(BOOT_DELAY);
@@ -57,9 +54,6 @@ void setup() {
   snprintf(identifier, sizeof(identifier), "VINDRIKTNING-%X", ESP.getChipId());
   u8g2_uint_t w = u8g2.getStrWidth(identifier);
 
-  /* u8g2.clearBuffer(); */
-  /* u8g2.drawStr((128 - w) / 2, (64 - 8) / 2, identifier); */
-  /* u8g2.sendBuffer(); */
   u8g2.setFont(u8g2_font_fub49_tn);
   Serial.printf("%s/%s/status\n", FIRMWARE_PREFIX, identifier);
 
@@ -93,27 +87,19 @@ void loop() {
       snprintf(humidity, sizeof(humidity), "%d%%", int(h));
       u8g2.drawStr(128 - u8g2.getStrWidth(fahrenheit) - 8, 8 + 3, fahrenheit);
       // degree https://github.com/olikraus/u8g2/wiki/u8g2reference#drawcircle
-      u8g2.drawCircle(128 - u8g2.getStrWidth(fahrenheit) * .333 - 8 - 1 , 2, 1, U8G2_DRAW_ALL);
+      u8g2.drawCircle(128 - u8g2.getStrWidth(fahrenheit) * 0.333 - 8 - 1, 2, 1, U8G2_DRAW_ALL);
       u8g2.drawStr(0, 8 + 3, humidity);
     }
-
-    /* u8g2.drawPixel(0,0); */
-    /* u8g2.drawPixel(124, 63); */
-    /* u8g2.drawPixel(0,63); */
-    /* u8g2.drawPixel(127,0); */
 
     if (state.valid) {
       u8g2.setFont(u8g2_font_fub49_tn);
       snprintf(avgPM25, sizeof(avgPM25), "%d", state.avgPM25);
       u8g2_uint_t w = u8g2.getStrWidth(avgPM25);
-      bool isMoreThan2Digits = true;//state.avgPM25 > 99;
-      int digitHeight = isMoreThan2Digits ? 62: 50;
+      int digitHeight = 62;
       u8g2.drawStr((128 - w) / 2, (64 - digitHeight) / 2 + digitHeight, avgPM25);
-      /* u8g2.drawStr((128 - w) / 2, (64 - 50) / 2 + 50, avgPM25); */
 
       circBuf.push(state.avgPM25);
     } else if (currentMillis < PARTICLE_SENSOR_LOADING_DELAY) {
-      /* circBuf.push(currentMillis / 1000 % 126); */
       u8g2.setFont(u8g2_font_6x12_tr);
       u8g2.drawStr(0, 38, "Air sensor loading");
     } else {
@@ -122,10 +108,10 @@ void loop() {
     }
 
     for (decltype(circBuf)::index_t i = 0; i < circBuf.size(); i++) {
-      /* u8g2.drawPixel(126 - i, 64 - circBuf[i]); */
-      if (i != 0)
+      if (i != 0) {
         u8g2.drawLine(i,     63 - circBuf[i],
                       i - 1, 63 - circBuf[i - 1]);
+      }
     }
 
     u8g2.sendBuffer();
